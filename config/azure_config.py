e505af168d4af3dd1722bd1e9e18f7eea8b0f30a
Doua Ben Rejeb <douaabnr15@gmail.com>
2026-02-19 13:39:40 +0100

    Create Azure configuration file with encryption settings and zero-trust policies

---
+++ config/azure_config.py
@@ -0,0 +1,10 @@
+# Azure Configuration
+
+# Encryption Settings
+ENCRYPTION_KEY = 'your-encryption-key'
+
+# Zero-Trust Policies
+ZERO_TRUST_POLICIES = {
+    'policy1': 'description',
+    'policy2': 'description',
+}
